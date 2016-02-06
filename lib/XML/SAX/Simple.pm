bc880402ad2b5a4c1ea2d67d04ebda88d5051d41
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2016-02-06 16:23:46 +0000

    - Refactored importing modules. - Tidied up pod document.

--- lib/XML/SAX/Simple.pm
+++ lib/XML/SAX/Simple.pm
@@ -2,7 +2,7 @@ package XML::SAX::Simple;
 
 =head1 NAME
 
-XML::SAX::Simple - SAX version of XML::Simple
+XML::SAX::Simple - SAX version of XML::Simple.
 
 =cut
 
@@ -12,36 +12,34 @@ use strict;
 use warnings;
 use Data::Dumper;
 
-use vars qw(@ISA @EXPORT);
-use XML::Simple ();
+use vars qw(@EXPORT);
 use XML::SAX;
 use XML::Handler::Trees;
-
-@ISA    = ('XML::Simple');
+use base 'XML::Simple';
 @EXPORT = qw(XMLin XMLout);
 
 =head1 DESCRIPTION
 
-XML::SAX::Simple is a very simple version of XML::Simple but for
-SAX. It can be used as a complete drop-in replacement for XML::Simple.
+C<XML::SAX::Simple> is a very simple version of L<XML::Simple> but for SAX.It can
+be used as a complete drop-in replacement for L<XML::Simple>.
 
-See the documentation for XML::Simple (which is required for this module
-to work) for details.
+See the documentation for L<XML::Simple> for details.
 
 =head1 SYNOPSIS
 
-  use XML::SAX::Simple qw(XMLin XMLout);
-  my $hash = XMLin("foo.xml");
+    use XML::SAX::Simple qw(XMLin XMLout);
+    my $hash = XMLin("foo.xml");
 
 =cut
 
 sub XMLin {
     my $self;
-    if($_[0] and UNIVERSAL::isa($_[0], 'XML::Simple')) {
+
+    if ($_[0] and UNIVERSAL::isa($_[0], 'XML::Simple')) {
         $self = shift;
     }
     else {
-        $self = new XML::SAX::Simple();
+        $self = XML::SAX::Simple->new();
     }
 
     $self->SUPER::XMLin(@_);
@@ -54,15 +52,16 @@ sub XMLout {
         $self = shift;
     }
     else {
-        $self = new XML::SAX::Simple();
+        $self = XML::SAX::Simple->new();
     }
+
     $self->SUPER::XMLout(@_);
 }
 
 sub build_tree {
-    my $self     = shift;
-    my $filename = shift;
-    my $string   = shift;
+    my ($self, $filename, $string) = @_;
+
+    $self->{nocollapse} = 1;
 
     if ($filename and $filename eq '-') {
         local($/);
@@ -73,8 +72,8 @@ sub build_tree {
     my $handler = XML::Handler::Tree->new();
     my $parser  = XML::SAX::ParserFactory->parser(Handler => $handler);
 
-    $self->{nocollapse} = 1;
     my $tree;
+
     if ($filename) {
         $tree = $parser->parse_uri($filename);
     }
@@ -93,6 +92,8 @@ sub build_tree {
 =head1 AUTHOR
 
 Matt Sergeant, matt@sergeant.org
+
+Currently maintained by Mohammad S Anwar (MANWAR), << <mohammad.anwar AT yahoo.com> >>
 
 =head1 SEE ALSO
 
